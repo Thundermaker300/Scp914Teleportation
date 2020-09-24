c80ce65b2d91ff8147d7efb9b354c6f662783c15
Thundermaker300 <thundermaker300@gmail.com>
2020-09-23 20:46:05 -0400

    fixing stuff

--- Scp914Teleportation/ClassLibrary1/Scp914Teleportation.cs
+++ Scp914Teleportation/ClassLibrary1/Scp914Teleportation.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace Scp914Teleportation
 {
-    public class Class1
+    public class Scp914Teleportation
     {
     }
 }
