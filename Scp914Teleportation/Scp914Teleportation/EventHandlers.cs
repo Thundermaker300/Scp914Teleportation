4190078d2b64ccd6ffe6c87cff13fa9bc8b4dd4f
Thundermaker300 <thundermaker300@gmail.com>
2020-09-23 21:01:56 -0400

    Setting up the framework for the plugin

---
+++ Scp914Teleportation/Scp914Teleportation/EventHandlers.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scp914Teleportation
+{
+    public class EventHandlers
+    {
+        public void OnUpgradingItems(UpgradingItemsEventArgs ev)
+        {
+
+        }
+    }
+}
--- Scp914Teleportation/Scp914Teleportation/Scp914Teleportation.cs
+++ Scp914Teleportation/Scp914Teleportation/Scp914Teleportation.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Events = Exiled.Events.Handlers;
+
 namespace Scp914Teleportation
 {
-    public class Scp914Teleportation
+    public class Scp914Teleportation : Plugin<Config>
     {
+        public static Scp914Teleportation Instance;
+
+        public override PluginPriority Priority { get; } = PluginPriority.Low;
+
+        private EventHandlers handler = new EventHandlers();
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            Instance = this;
+
+            // Handlers
+            Events.Scp914.UpgradingItems += handler.OnUpgradingItems;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            // Handlers
+            Events.Scp914.UpgradingItems -= handler.OnUpgradingItems;
+
+            // Goodbye
+            Instance = null;
+            handler = null;
+        }
     }
 }
