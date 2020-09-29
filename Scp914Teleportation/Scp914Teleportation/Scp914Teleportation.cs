2cac07c6a0cf8069e3ca2bae6c0166eb720d089f
Juliet <thundermaker300@gmail.com>
2020-09-29 14:37:46 -0400

    prepping for v1.4.0

--- Scp914Teleportation/Scp914Teleportation/Scp914Teleportation.cs
+++ Scp914Teleportation/Scp914Teleportation/Scp914Teleportation.cs
@@ -16,7 +16,7 @@ namespace Scp914Teleportation
     {
         public override string Name { get; } = "Scp914Teleportation";
         public override string Author { get; } = "Thunder";
-        public override Version Version { get; } = new Version(1, 3, 0);
+        public override Version Version { get; } = new Version(1, 4, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 6);
 
         public override string Prefix { get; } = "Scp914Teleportation";
